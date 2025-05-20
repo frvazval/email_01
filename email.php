af4826cbb46cd7a9d76c64e3f793e3efb267ff87
Fvazquez <foapgenerico@365.stucom.com>
2025-05-20 16:17:40 +0200

    stucom

---
+++ email.php
@@ -0,0 +1,50 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'vendor/PHPMailer/src/Exception.php';
+require 'vendor/PHPMailer/src/PHPMailer.php';
+require 'vendor/PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0; //SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'frvazval@gmail.com';                     //SMTP username
+    $mail->Password   = 'mwqpwzlhjkeybdoz';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('frvazval@gmail.com', 'Francisco Vazquez');  // Remitente
+    $mail->addAddress('frvazval@gmail.com', 'Francisco Vazquez');     //Receptor
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->CharSet = 'UTF-8'; // Set the character set to UTF-8
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'CORREO DE PRUEBA desde PHP'; // Asunto
+    $mail->Body    = '<h1>Hola</h1><p>Este es un correo de prueba desde PHP</p>'; // Cuerpo del mensaje
+    $mail->AltBody = 'Esto es una prueba de envio de correo desde PHP'; // Texto alternativo para clientes de correo que no soportan HTML
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
